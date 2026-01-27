9b003cfe62bf129df7860d8c1a3ee0cfe544776b
Tom TREILLON <TREILLON_To@lprs.local>
2026-01-27 16:28:03 +0100

    [feat]Début du projet et préparation de la barre de navigation

---
+++ src/barreDeNavigation.php
@@ -0,0 +1,26 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+
+<header class="navigation">
+    <nav>
+        <ul>
+            <li>Bien-être Étudiants</li>
+            <li><a href="">Stress</a></li>
+            <li><a href="">Isolement</a></li>
+            <li><a href="">Désinformation</a></li>
+            <li><a href="">Handicap</a></li>
+            <li><a href="">Communication</a></li>
+            <li></li>
+            <li></li>
+        </ul>
+    </nav>
+</header>
+
+</body>
+
+</html>
